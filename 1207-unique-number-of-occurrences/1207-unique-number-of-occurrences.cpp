3a6d5ca430bdc2a3c461aa7c9320762ec2fb9bd5
akhilsrivatsa <62267373+akhilsrivatsa@users.noreply.github.com>
2024-01-17 02:51:07 -0500

    Time: 0 ms (100.00%), Space: 8.9 MB (9.43%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,27 +3,18 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         
         unordered_map<int, int> mp;
-        for(int i = 0; i<arr.size(); i++) {
-            //storing the number of occurances of elements
+        for(int i = 0; i < arr.size(); i++){
             mp[arr[i]]++;
         }
-
-        vector<int> freq;
-        for(auto i:mp) {
-            //pushing the occurances into frequency vector
-            freq.push_back(i.second);
-        }
-
-        //sorting the frequency vector
-        sort(freq.begin(), freq.end());
-
-        for(int i = 0; i<freq.size()-1; i++) {
-            //checking if 2 consequent elements are equal
-            if(freq[i] == freq[i+1])
+        set<int> s;
+        for(auto it = mp.begin(); it != mp.end() ;it++){
+            
+            if(s.find(it->second) != s.end()){
                 return false;
-        }
+            }
+            s.insert(it -> second);
+        }   
 
         return true;
-
     }
 };
